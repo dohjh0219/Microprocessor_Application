bed8a7c8b11d9551b0257a2285d3aebf7e98ce27
Doh Jun Hyeong <dohjh0219@gmail.com>
2026-05-05 16:10:39 +0900

    Week 9 Update

--- Week9_Timer_Clock/Code1/Code1.ino
+++ Week9_Timer_Clock/Code1/Code1.ino
@@ -5,33 +5,28 @@
 #define set_bit(value, bit) ( _SFR_BYTE(value) |= _BV(bit) )
 #define clear_bit(value, bit) ( _SFR_BYTE(value) &= ~_BV(bit) )
 
-volatile int count = 0; 
-uint8_t state = 0;
+volatile int count = 0;
+uint8_t state = 0; 
 
-ISR(TIMER0_COMPA_vect) {
+ISR(TIMER0_OVF_vect) {
   count++;
-  TCNT0 = 0;
 }
 
 int main(void) {
-  set_bit(DDRB, 7); 
-  clear_bit(PORTB, 7); 
+  set_bit(DDRB, 7);
+  clear_bit(PORTB, 7);
 
   TCCR0B |= (1 << CS02) | (1 << CS00);
-
-  TIMSK0 |= (1 << OCIE0A);
-  OCR0A = 128;
-
-  sei();
+  TIMSK0 |= (1 << TOIE0);
+  sei(); 
 
   while (1) {
-    if (count == 64) {
-      count = 0;
-      state = !state;
-      if (state) set_bit(PORTB, 7); 
+    if (count == 32) {  
+      count = 0; 
+      state = !state; 
+      if (state) set_bit(PORTB, 7);
       else clear_bit(PORTB, 7);
     }
   }
-
   return 0;
 }
